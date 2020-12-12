5c487c184521cac37a39854c4f3cd3bfc98e7f47
Shakthi <sgg329@nyu.edu>
2020-12-12 17:54:48 -0500

    Base backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,18 @@
+  
 const express = require("express");
-const router = express.Router();
 
-router.get("/", (req,res) => res.send('Testing backend'));
+const router = express.Router();
+const sampleJSON = [{
+    name: 'Shakthi',
+    role: 'Student',
+    dog: 'Boba'
+},
+{
+    name: 'Ell',
+    role: 'Student',
+    dog: 'Kitty'
+}
+]
+router.get('/', (req,res) => res.send(sampleJSON));
 
 module.exports = router;
